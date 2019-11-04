ee1570087c486199f8982afc01d18e9736423579
rkist <raulkist@gmail.com>
2019-11-04 18:11:43 -0300

    WIP

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -53,13 +53,12 @@ done
 export CATALINA_OPTS="$(cat ./catalina_opts) $@"
 echo $CATALINA_OPTS
 
-# EXTRA_SCRIPT=extra_script.sh
-# if [ -x $EXTRA_SCRIPT ]
-# then
-# 	echo "Executing " $EXTRA_SCRIPT
-# 	exec $EXTRA_SCRIPT
-# fi
-
+EXTRA_SCRIPT=extra_script.sh
+if [ -x $EXTRA_SCRIPT ]
+then
+	echo "Executing " $EXTRA_SCRIPT
+	exec $EXTRA_SCRIPT
+fi
 
 echo "=========================="
 echo ${JDBC_URL}
